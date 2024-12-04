7ba07afbe9387976f58799ec9b02923b7b71380c
Laahiri07 <lkomatham@slu.edu>
2024-12-04 00:54:57 +0000

    lab-6

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function Search({handleSearch}) {
+export default function Search({ onFilter }) {
+  const [search, setSearch] = useState('');
 
-  // This is a controlled component, so we need to keep track of the search term in state
-  const [search, setSearch] = React.useState('')
-
-  // This function will be called whenever the user types in the search box
   const handleChange = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value); // Update the search term as the user types
+  };
 
-  // This function will be called when the user submits the form
   const handleSubmit = (e) => {
-    // Prevent the default behaviour of the form, which is to refresh the page
-    e.preventDefault()
-    // Call the handleSearch function that was passed in as a prop; this will pass the search term back up to the parent component (CardList) where it can be used to filter the cards
-    handleSearch(search.toLowerCase())
-  }
+    e.preventDefault(); // Prevent the default form submit behavior (page reload)
+    onFilter(search.toLowerCase()); // Pass the search term to the parent component's filter function
+  };
 
   return (
-    <form class="pt4 pb4 pl2 black-80" onSubmit={handleSubmit}>
-      <fieldset class="cf bn ma0 pa0">
-        <div class="cf measure mb2">
-          <input class="f6 f5-l input-reset fl black-80 ba b--black-20 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder='Tag Search' value={search} onChange={handleChange} type="text" />
-          <input class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Search" />
+    <form className="pt4 pb4 pl2 black-80" onSubmit={handleSubmit}>
+      <fieldset className="cf bn ma0 pa0">
+        <div className="cf measure mb2">
+          <input
+            className="f6 f5-l input-reset fl black-80 ba b--black-20 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
+            placeholder="Tag Search"
+            value={search}
+            onChange={handleChange}
+            type="text"
+          />
+          <button
+            type="submit"
+            className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
+          >
+            Search
+          </button>
         </div>
-        <small id="name-desc" class="f6 black-60 db mb2">Enter a keyword to filter by tags</small>
-    </fieldset>
+        <small id="name-desc" className="f6 black-60 db mb2">
+          Enter a keyword to filter by tags
+        </small>
+      </fieldset>
     </form>
-  )
+  );
 }
