9b48b95a79d084d574d50a9aeb9f47324ef1b48b
Jagadeesh Gudidhi <jagadeesh.gudidhi@outlook.com>
2024-12-04 05:27:39 +0000

    Week 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import Header from './components/Header';
 import productData from './data/full-products';
-
+import CardList from './components/CardList';
+import SingleView from './components/SingleView';
+import { Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h1>Hello There.</h1>
+      <Header />
+        <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
+      
     </div>
   );
 }
-
 export default App;
